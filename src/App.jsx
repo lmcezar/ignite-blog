023893e8bf6da79b3f9d3aeea3dfed71fb39b468
Lucas Cezar <lmcezar@gmail.com>
2023-07-17 08:44:36 -0300

    Estilizações do Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Post } from "./Post";
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 import "./global.css";
 import styles from "./App.module.css";
@@ -12,6 +12,10 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
+          <Post
+            author="Lucas Cezar"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius rhoncus massa, vel convallis nisi pharetra vitae. Proin eget tincidunt orci. Curabitur eget turpis luctus, sollicitudin neque sed, pulvinar nunc. Sed convallis tellus et justo vulputate lacinia. Duis dignissim urna nec erat condimentum, vitae tincidunt purus commodo. Duis in viverra est. Integer molestie venenatis nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse tristique tristique scelerisque. Nunc nec dolor sed leo consectetur ultricies ut eget sem."
+          />
           <Post
             author="Lucas Cezar"
             content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius rhoncus massa, vel convallis nisi pharetra vitae. Proin eget tincidunt orci. Curabitur eget turpis luctus, sollicitudin neque sed, pulvinar nunc. Sed convallis tellus et justo vulputate lacinia. Duis dignissim urna nec erat condimentum, vitae tincidunt purus commodo. Duis in viverra est. Integer molestie venenatis nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse tristique tristique scelerisque. Nunc nec dolor sed leo consectetur ultricies ut eget sem."
---
+++ src/components/Post.jsx
@@ -0,0 +1,39 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://media.licdn.com/dms/image/D4E03AQESZsaij75OIA/profile-displayphoto-shrink_100_100/0/1688649889638?e=1695254400&v=beta&t=Y4XeglpQMtBWOGe4lcvC2PQQh527JS3s7OybsHE9guY"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Lucas Cezar</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="16 de julho as 09:10" dateTime="2023-07-16 09:10:30">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
