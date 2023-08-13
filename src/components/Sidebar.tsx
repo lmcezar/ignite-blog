ce8fd9b155a472cc57af431a8e13d1e83e281041
Lucas Cezar <lmcezar@gmail.com>
2023-08-13 10:33:05 -0300

    TypeScript no React

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { PencilLine } from "@phosphor-icons/react";
+
+import styles from "./Sidebar.module.css";
+import { Avatar } from "./Avatar";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+        alt=""
+      />
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/lmcezar.png" />
+
+        <strong>Lucas Cezar</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
